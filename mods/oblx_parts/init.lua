7693f2e638d4783223e213bc58107413b143a525
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-08-09 19:35:17 +0200

    Add more colored parts.

--- mods/oblx_parts/init.lua
+++ mods/oblx_parts/init.lua
@@ -1,8 +1,36 @@
 
--- TODO: this is just a temporary node, it should be colorized from a white node.
-minetest.register_node('oblx_parts:red', {
-    description = 'Red Part',
-    tiles = {"oblx_parts_red_outset.png", "oblx_parts_red.png"},
-    groups = { oddly_breakable_by_hand = 3 },
-    is_ground_content = true
-})
+colors = {
+	{
+		id = "red",
+		name = "Red",
+		color = "ff2222"
+	}, {
+		id = "yellow",
+		name = "Yellow",
+		color = "ffff22"
+	}, {
+		id = "green",
+		name = "Green",
+		color = "22ff22"
+	}, {
+		id = "blue",
+		name = "Blue",
+		color = "2222ff"
+	}, {
+		id = "gray",
+		name = "Gray",
+		color = "ffffff"
+	},
+}
+
+for _, color in pairs(colors) do
+	minetest.register_node('oblx_parts:'..color.id, {
+		description = color.name..' Part',
+		tiles = {
+			"oblx_parts_outset.png^[multiply:#"..color.color,
+			"oblx_parts_smooth.png^[multiply:#"..color.color
+		},
+		groups = { oddly_breakable_by_hand = 3 },
+		is_ground_content = true
+	})
+end
