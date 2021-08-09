73bb5b30e6a47f693da0ef9b72ed08c4fed8d144
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-08-09 19:35:43 +0200

    Singlenode mapgen is now a standard baseplate.

---
+++ mods/oblx_baseplate/init.lua
@@ -0,0 +1,40 @@
+
+
+mg = {
+	blocks = {
+		gay = minetest.get_content_id("oblx_parts:gray"),
+	},
+	size = 256,
+	depth = 20
+}
+
+data = {}
+
+if minetest.get_mapgen_setting('mg_name') == "singlenode" then
+	minetest.register_on_generated(function(minp, maxp, blockseed)
+		local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
+		local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
+		vm:get_data(data)
+
+		for z = 0, 79 do
+			for y = 0, 79 do
+				for x = 0, 79 do
+					pos = {
+						x = minp.x + x,
+						y = minp.y + y,
+						z = minp.z + z
+					}
+
+					posi = area:index(pos.x, pos.y, pos.z)
+
+					if (pos.x >= -mg.size and pos.x <= mg.size) and (pos.z >= -mg.size and pos.z <= mg.size) and (pos.y >= -mg.depth and pos.y <= 0) then
+						data[posi] = mg.blocks.gay
+					end
+				end
+			end
+		end
+
+		vm:set_data(data)
+		vm:write_to_map()
+	end)
+end
