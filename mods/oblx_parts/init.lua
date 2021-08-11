57ce841de202c672a756083babefecf0382fb658
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-08-11 11:46:22 +0200

    There's no need for two for loops that do the same thing.

--- mods/oblx_parts/init.lua
+++ mods/oblx_parts/init.lua
@@ -142,9 +142,7 @@ for _, color in pairs(colors) do
 		groups = { oddly_breakable_by_hand = 3 },
 		is_ground_content = true
 	})
-end
 
-for _, color in pairs(colors) do
     minetest.register_node('oblx_parts:'..color.id..'_wedge', {
         description = color.name..' Wedge Part',
         tiles = {"oblx_parts_smooth.png^[multiply:#"..color.color},
