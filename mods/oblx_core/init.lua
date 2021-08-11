2428c8f0f4a6cdf7474ffaa1ddbfbd6f3fa3eb09
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-08-11 22:55:31 +0200

    Raise range because why not.

--- mods/oblx_core/init.lua
+++ mods/oblx_core/init.lua
@@ -8,7 +8,7 @@ minetest.register_item(':', {
 	type = 'none',
 	wield_image = 'oblx_core_hand.png',
 	wield_scale = {x = 0.5, y = 1, z = 4},
-	range = 10,
+	range = 25,
 	tool_capabilities = {
 		full_punch_interval = 0.9,
 		max_drop_level = 0,
