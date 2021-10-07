3ceff8d4d021d45eac37a2059e587a10e5a5170a
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-10-07 21:20:17 +0200

    Add woodlands biome.

--- mods/oblx_mapgen/init.lua
+++ mods/oblx_mapgen/init.lua
@@ -13,8 +13,22 @@ minetest.register_biome({
 	depth_riverbed = 2,
 	y_max = 31000,
 	y_min = 4,
-	heat_point = 50,
-	humidity_point = 12,
+	heat_point = 37.4,
+	humidity_point = 34.5,
+})
+
+minetest.register_biome({
+	name = "woodlands",
+	node_top = "oblx_parts:dark_green",
+	depth_top = 1,
+	node_filler = "oblx_parts:brown",
+	depth_filler = 3,
+	node_riverbed = "oblx_parts:cool_yellow",
+	depth_riverbed = 2,
+	y_max = 31000,
+	y_min = 4,
+	heat_point = 82.7,
+	humidity_point = 40.7,
 })
 
 minetest.register_biome({
@@ -50,6 +64,20 @@ minetest.register_decoration({
 	sidelen = 16,
 	fill_ratio = 0.0001,
 	biomes = {"grasslands"},
+	y_max = 31000,
+	y_min = 1,
+	schematic = minetest.get_modpath("oblx_mapgen").."/schematics/tree_test.mts",
+	flags = "place_center_x, place_center_z",
+	rotation = "random",
+})
+
+minetest.register_decoration({
+	name = "oblx_mapgen:tree_test2",
+	deco_type = "schematic",
+	place_on = {"oblx_parts:dark_green"},
+	sidelen = 16,
+	fill_ratio = 0.005,
+	biomes = {"woodlands"},
 	y_max = 31000,
 	y_min = 1,
 	schematic = minetest.get_modpath("oblx_mapgen").."/schematics/tree_test.mts",
